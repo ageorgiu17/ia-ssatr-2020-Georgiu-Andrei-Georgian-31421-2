99ac6aa576737ec961e8be342ef19dd85e71a656
Andrei Georgiu <georgiuandrei05@gmail.com>
2020-11-09 20:07:18 +0200

    Laborator 2 ex 5

---
+++ bank-sim/src/main/java/AccountsManager.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gabriel
+ */
+public class AccountsManager {
+    BankAccount[] list = new BankAccount[10];
+    
+    void addAccount(BankAccount a){
+    for(int i=0; i<list.length; i++){
+    if(list[i]==null) {
+    list[i] = a;
+    System.out.println("New account added");
+    return;}}
+    System.out.println("No empty position fonud");
+    }
+ 
+    int getTotalBalance(){
+        int b = 0;
+        int k = 0;
+        for(BankAccount a: list) {
+            if(a!=null) {
+            b+=a.getBalance();
+            k++;
+       
+            }
+        }
+        return b/k;
+    }
+}
